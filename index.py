f6f6d7af23620a4d557c020064170d15fb5989e1
spectralo <arthur.hardy@mageekbox.net>
2024-09-22 12:01:17 +0000

    Auth working

--- index.py
+++ index.py
@@ -1,4 +1,8 @@
 import os
+import requests
+
+schoolid = ""
+token = ""
 
 if os.getenv("TOKEN","bad") == "bad":
     print("You need to export your token as a env var")
@@ -7,3 +11,15 @@ else:
     token = os.getenv("TOKEN")
     print("Token found !")
     print("Your token is : "+token)
+
+if os.getenv("SCHOOLID","bad") == "bad":
+    print("You need to export your SCHOOLID as a env var")
+    print('On Linux/MacOS : export SCHOOLID="schoolid"')
+else:
+    schoolid = os.getenv("SCHOOLID")
+    print("Schoolid found !")
+    print("Your schoolid is : "+schoolid)
+
+session = requests.Session()
+r = session.get('https://webparent.paiementdp.com/aliAuthentification.php?site='+schoolid+'&token='+token)
+print(session.cookies.get_dict())
